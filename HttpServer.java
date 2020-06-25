5c8c67b17c0bede7bf14ea7bed723e72ed138029
daniel <47526269+shinatsuhiko@users.noreply.github.com>
2020-06-25 20:47:57 +0200

    Add files via upload

--- HttpServer.java
+++ HttpServer.java
@@ -4,6 +4,27 @@ import java.io.*;
 import java.net.*;
 
 public class HttpServer{
+
+      public void crearHilos(int pServer, String ipControl, int pControl, int conex){
+        int conexionesActuales = 0;
+        try {
+            ServerSocket sockServidor = new ServerSocket(pServer);
+            System.out.println("Escuchando por el puerto: " +pServer);
+            for(;;){
+                conexionesActuales++;
+                Socket cliente = sockServidor.accept();
+                Thread t = new ServerHilo(cliente,ipControl,pControl);
+                t.start();
+                conexionesActuales--;
+            }
+        }
+        catch (Exception ex) {
+            System.err.println("Error con la conexión: " + ex);
+        }
+    }
+
+
+
     public static void main(String[] args){
         HttpServer myHTTPServer = new HttpServer();
         int puertoServer, puertoController, conexiones;
@@ -13,35 +34,19 @@ public class HttpServer{
                 System.out.println("Introduzca los argumentos en el siguiente orden: puerto servidor, ip controlador, puerto controlador y número máximo de conexiones");
             }
             else {
-                puertoServer = args[0];
+                puertoServer = Integer.parseInt(args[0]);
                 ipController = args[1];
-                puertoController = args[2];
-                conexiones = args[3];
+                puertoController = Integer.parseInt(args[2]);
+                conexiones = Integer.parseInt(args[3]);
+                myHTTPServer.crearHilos(puertoServer,ipController,puertoController,conexiones);
+
             }
-            catch (IOException | NumberFormatException ex) {
+            }catch (NumberFormatException ex) {
                 System.err.println("Error en los argumentos, el orden es: puerto servidor, ip controlador, puerto controlado y número máximo de conexiones " + ex);
             }
-        }
         
-        HttpServer.crearHilos(puertoServer,ipController,puertoController,conexiones);
     }
     
-    public void crearHilos(int pServer, String ipControl, int pControl, int conex){
-        int conexionesActuales = 0;
-        try {
-            ServerSocket sockServidor = new ServerSocket(pServer);
-            System.out.println("Escuchando por el puerto: " +pServer);
-            for(;;){
-                conexionesActuales++;
-                Socket cliente = sockServidor.accept();
-                Thread t = new HiloServidor(cliente,ipControl,pControl);
-                t.start();
-                conexionesActuales--;
-            }
-        }
-        catch (Exception ex) {
-            System.err.println("Error con la conexión: " + ex);
-        }
-    }
+  
 }
         
